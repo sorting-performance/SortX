ab6165044fc39fcd11c28a09c293e9945cf10e64
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2025-11-22 20:03:21 +0530

    csv file

--- src/main/java/com/example/sortx/HelloController.java
+++ src/main/java/com/example/sortx/HelloController.java
@@ -1,8 +1,21 @@
 package com.example.sortx;
 
+import javafx.stage.FileChooser;
+import java.io.File;
 
 public class HelloController {
     public void buttonClick(){
-        System.out.println("Sorting is Started");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select CSV File");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("CSV Files", "*.csv")
+        );
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+            System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+            // do something with the file
+        }
     }
 }
