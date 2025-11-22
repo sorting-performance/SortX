f8d196e9e8f4a4e60aa30ac7661c309b2d573f92
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2025-11-22 19:35:45 +0530

    resolution fix

--- src/main/java/com/example/sortx/HelloApplication.java
+++ src/main/java/com/example/sortx/HelloApplication.java
@@ -10,7 +10,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setResizable(false);
         stage.setTitle("SortX");
         stage.setScene(scene);
