c19b5e01562b182e909d3a4b6cbe0d38b85f07d5
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2025-11-24 00:12:49 +0530

    quickSort
    partition also implemented

--- src/main/java/com/example/sortx/HelloController.java
+++ src/main/java/com/example/sortx/HelloController.java
@@ -5,4 +5,43 @@ public class HelloController {
     public void buttonClick(){
         System.out.println("Sorting is Started");
     }
+
+
+
+
+
+
+
+
+
+
+    // -------- Sorting Algorithms --------
+
+
+    private void quickSort(double[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
+
+    private int partition(double[] arr, int low, int high) {
+        double pivot = arr[high];
+        int i = low -1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                double temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        double temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+
+
 }
