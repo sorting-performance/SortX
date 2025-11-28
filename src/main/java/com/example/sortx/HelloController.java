44335a4899d4a70ed248e815b74f6fce7ac86050
N Anushan <nanushan8@gmail.com>
2025-11-28 22:34:59 +0530

    merge sort
    Co-Authored-By: Sanchitha Udana Wijesundara <109952575+sanchithaudana@users.noreply.github.com>
    Co-Authored-By: kasun Isuru <72108374+KasunIsuru@users.noreply.github.com>

--- src/main/java/com/example/sortx/HelloController.java
+++ src/main/java/com/example/sortx/HelloController.java
@@ -132,6 +132,32 @@ public class HelloController {
         }
     }
 
+    private void mergeSort(double[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right)/2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid+1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private void merge(double[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        double[] L = new double[n1];
+        double[] R = new double[n2];
+        for (int i=0; i<n1; i++) L[i] = arr[left+i];
+        for (int j=0; j<n2; j++) R[j] = arr[mid+1+j];
+
+        int i=0, j=0, k=left;
+        while(i<n1 && j<n2) {
+            if (L[i] <= R[j]) arr[k++] = L[i++];
+            else arr[k++] = R[j++];
+        }
+        while(i<n1) arr[k++] = L[i++];
+        while(j<n2) arr[k++] = R[j++];
+    }
+
     private int partition(double[] arr, int low, int high) {
         double pivot = arr[high];
         int i = low -1;
